6a76bfdeb7b4eba9a6810327b98c59a5ab700a85
SvenVanDerMeij <38007@ma-web.nl>
2024-10-02 15:14:03 +0200

    sprint 2

--- TowerdefenseJaar2M1/Assets/Scripts/Enemies/Movement/EnemyMovement.cs
+++ TowerdefenseJaar2M1/Assets/Scripts/Enemies/Movement/EnemyMovement.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
@@ -7,26 +8,33 @@ public class EnemyMovement : MonoBehaviour
     private bool moving = true;
     public List<GameObject> allpoints = new List<GameObject>();
     public List<GameObject> adjacent;
+    public List<GameObject> deathPoints;
+    public List<GameObject> cashPoints;
     public GameObject firstPoint;
     public GameObject currentTarget;
     public float speed = 4f;
+    static int cash = 0;
+    static int lives = 100;
 
     Vector3 differenceVector;
-    
+    Vector3 rotation;
     Vector3 Direction;
     Vector3 velocity;
     bool ticked;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-       float currentDistance = Vector3.Distance(currentTarget.transform.position, transform.position);
-       Debug.Log(adjacent.Count);
+
+        float currentDistance = Vector3.Distance(currentTarget.transform.position, transform.position);
+        Vector3 rotation = currentTarget.transform.position - transform.position;
+        // float currentDeathstance = Vector3.Distance(go.transform.position);
+       // Debug.Log(adjacent.Count);
         if (moving == true)
         {
             differenceVector = currentTarget.transform.position - transform.position;
@@ -34,32 +42,48 @@ public class EnemyMovement : MonoBehaviour
             velocity = Direction * speed * Time.deltaTime;
             transform.position += velocity;
         }
-        
-            if (currentDistance < 0.2f)
-            {
-                moving = false;       
-                adjacent.Clear();
-            }
-            if (adjacent.Count == 0) 
-             {
-               checkAdjacent();
-             }
-            
-        
-       
-        
-        void checkAdjacent()
+
+        if (currentDistance < 0.1f)
         {
-            foreach (GameObject point in allpoints)
+            transform.position = currentTarget.transform.position;
+            moving = false;
+            adjacent.Clear();
+            checkLifeOrDeath();
+        }
+        if (adjacent.Count == 0)
+        {
+            checkAdjacent();
+        }
+        float newHeading = Mathf.Atan2(differenceVector.y, differenceVector.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, 180+newHeading);
+
+
+    }
+    void checkAdjacent()
+    {
+        foreach (GameObject point in allpoints)
+        {
+            if (point.transform.position.y + .6f > transform.position.y && point.transform.position.y - .6f < transform.position.y || point.transform.position.x + .6f > transform.position.x && point.transform.position.x - .6f < transform.position.x)
             {
-                if (point.transform.position.y + .6f > transform.position.y && point.transform.position.y - .6f < transform.position.y || point.transform.position.x + .6f > transform.position.x && point.transform.position.x - .6f < transform.position.x)
-                {
-                    adjacent.Add(point);
-                    currentTarget = adjacent[Random.Range(0, adjacent.Count)];
-                    moving = true;
-                }
-            }
+                adjacent.Add(point);
+                currentTarget = adjacent[Random.Range(0, adjacent.Count)];
+                moving = true;
             }
         }
     }
+    void checkLifeOrDeath()
+    {
+        if (transform.position.x > 8.5f)
+        {
+            lives -= 1;
+            Destroy(gameObject);
+        }
+        if (transform.position.x < -8.5f)
+        {
+            cash += 1;
+            Destroy(gameObject);
+        }
+    }
+
+}
 
--- TowerdefenseJaar2M1/Assets/Scripts/Enemies/Spawning/enemySpawner.cs
+++ TowerdefenseJaar2M1/Assets/Scripts/Enemies/Spawning/enemySpawner.cs
@@ -1,27 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class enemySpawner : MonoBehaviour
 {
-    [SerializeField]  private List<GameObject> allpoints = new List<GameObject>();
-    [SerializeField]  private List<GameObject> adjacent = new List<GameObject>();
+    private float time = 0;
+    [SerializeField] private List<GameObject> allpoints = new List<GameObject>();
+    [SerializeField] private List<GameObject> adjacent = new List<GameObject>();
+    [SerializeField] private List<GameObject> deathPoints = new List<GameObject>();
+    [SerializeField] private List<GameObject> cashPoints = new List<GameObject>();
+    [SerializeField] private GameObject firstTarget;
     [SerializeField] GameObject enemy;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        GameObject currentEnemy =Instantiate(enemy);
+        GameObject currentEnemy = Instantiate(enemy);
+        currentEnemy.GetComponent<EnemyMovement>().deathPoints = deathPoints;
+        currentEnemy.GetComponent<EnemyMovement>().cashPoints = cashPoints;
         currentEnemy.GetComponent<EnemyMovement>().allpoints = allpoints;
-        currentEnemy.GetComponent <EnemyMovement>().adjacent = adjacent;
-        currentEnemy.GetComponent<EnemyMovement>().currentTarget = adjacent[Random.Range(0, adjacent.Count)];
+        currentEnemy.GetComponent<EnemyMovement>().adjacent = adjacent;
+        currentEnemy.GetComponent<EnemyMovement>().currentTarget = firstTarget;
+        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        
+        time += Time.deltaTime;
+        if (time > 1) 
+        {
+            GameObject currentEnemy = Instantiate (enemy);
+            currentEnemy.GetComponent<EnemyMovement>().deathPoints = deathPoints;
+            currentEnemy.GetComponent<EnemyMovement>().cashPoints = cashPoints;
+            currentEnemy.GetComponent<EnemyMovement>().allpoints = allpoints;
+            currentEnemy.GetComponent<EnemyMovement>().adjacent = adjacent;
+            currentEnemy.GetComponent<EnemyMovement>().currentTarget = firstTarget;
+            time = 0;
+        }
+       
     }
 }
